10184837e0a7a9b4b0dddc849065955fb949b796
Dmjmg <dm_jmg@mail.ru>
2024-04-29 00:51:13 +0300

    Написали решение поставленной задачи

---
+++ ControlApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ControlApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = { "Hello", "2", "world", ":-)" };
+            Console.WriteLine(inputArray);
+
+            string[] outputArray = inputArray.Where(s => s.Length <= 3).ToArray();
+
+            Console.WriteLine();
+
+            foreach (string s in outputArray)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    }
+
